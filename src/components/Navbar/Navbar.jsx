476bcc98a4b41a2eccc0727beacfc3f87fea8d2a
OmkarK3030 <omkabugade@gmail.com>
2025-05-28 23:56:54 +0530

    Updated Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 
   const menuItems = [
     { id: "about", label: "About" },
-    { id: "skills", label: "Skills" },
+    // { id: "skills", label: "Skills" },
     { id: "experience", label: "Experience" },
     { id: "work", label: "Projects" },
     { id: "education", label: "Education" },
